a77b74b38e6a3b6f7d12ff8d92471c08fe0b0684
TABISH KHAN <65732747+tabishk111@users.noreply.github.com>
2022-10-05 13:00:49 +0530

    Add files via upload

--- src/Spinner/index.js
+++ src/Spinner/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React from 'react'
+import "../../styles/Sidebar.css";
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
+import CloudQueueIcon from '@material-ui/icons/CloudQueue';
+import NewFile from './NewFile';
+import SidebarItem from './SidebarItem';
 
-import './style.scss';
+function index( { user } ) {
+    return (
+        <div className="sidebar">
+            <NewFile user = {user}/>
 
-export default () => (
-  <div className="spinner lds-ring">
-    <div />
-    <div />
-    <div />
-    <div />
-  </div>
-);
+            <div className="sidebar__itemsContainer">
+                <SidebarItem arrow icon={(<InsertDriveFileIcon style={{fill: 'rgb(66,133,244)'}}/>)} label={'My Drive'} active/>
+                <SidebarItem arrow icon={(<ImportantDevicesIcon />)} label={'Computers'} />
+                <SidebarItem icon={(<PeopleAltIcon />)} label={'Shared with me'} />
+                <SidebarItem icon={(<QueryBuilderIcon />)} label={'Recent'} />
+                <SidebarItem icon={(<StarBorderIcon />)} label={'Starred'} />
+                <SidebarItem icon={(<DeleteOutlineIcon />)} label={'Bin'} />
+                
+                <hr/>
+                
+                <SidebarItem icon={(<CloudQueueIcon />)} label={'Storage'} />
+            </div>
+        </div>
+    )
+}
+
+export default index;
